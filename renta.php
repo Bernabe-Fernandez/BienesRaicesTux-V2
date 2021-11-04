c2d88c8956a3feded1ab60a6b663e5428464108f
Bernabe Fernandez <bernabefernandezg05@gmail.com>
2021-11-03 22:36:57 -0600

    se sube catalago de venta y renta

---
+++ renta.php
@@ -0,0 +1,35 @@
+<?php
+require 'includes/funciones.php';
+
+//crear mensaje de inserscion correcta a la bd
+$resultado = $_GET['resultado'] ?? null;
+
+
+incluirTemplate('header');
+?>
+
+
+<!-- seccion del catalago -->
+<section class="contenedor">
+    <div class="catalago-home">
+        <h2>
+            Casas y Departamentos Destacados
+        </h2>
+        <div class="catalago-home-container">
+            <!-- tarjeta de la casa publicada -->
+            <?php
+            include 'includes/templates/anunciosrentas.php';
+            ?>
+        </div>
+    </div>
+</section>
+<!-- footer -->
+<footer>
+    <p>Bienes<span>Raices</span>Tux Todos los Derechos Reservados &copy;</p>
+</footer>
+
+<!-- mandamos a llmar js -->
+<script src=" js/script.js"></script>
+</body>
+
+</html>
